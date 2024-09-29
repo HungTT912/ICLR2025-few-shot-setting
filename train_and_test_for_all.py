9bba1cfa9be8bb540cf3ea32ad338711fa04a8b9
HungTT912 <noohung2003@gmail.com>
2024-09-29 20:52:18 +0700

    a

--- train_and_test_for_all.py
+++ train_and_test_for_all.py
@@ -86,7 +86,7 @@ def tester(config,task):
 
 def main():
     nconfig, dconfig = parse_args_and_config()
-    wandb.init(project='BBDM',
+    wandb.init(project='BBDM-fewshot-setting',
             name=nconfig.wandb_name,
             config = dconfig) 
     args = nconfig.args
