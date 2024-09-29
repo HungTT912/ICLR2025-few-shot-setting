760cafa87ac8ee2aff74d63eea1c2065e262857d
HungTT912 <noohung2003@gmail.com>
2024-09-29 20:28:33 +0700

    a

--- train-bash.sh
+++ train-bash.sh
@@ -1,6 +1,5 @@
-python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s10000-l5.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s15000-l5.0-lr0.05-d0.25.yaml \
-&python3 test_tfbind8_tune_22.py --config configs/tune_22/Template-BBDM-tfbind8-s20000-l5.0-lr0.05-d0.25.yaml \
-&python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s8500-l5.0-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s9000-l5.0-lr0.05-d0.25.yaml --save_top \
-&python3 train_for_all.py --config configs/tune_22/Template-BBDM-tfbind8-s9500-l5.0-lr0.05-d0.25.yaml --save_top
+python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-ant-highest.yaml \
+&python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-dkitty-highest.yaml 
+
+python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-highest.yaml \
+&python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind10-highest.yaml 
