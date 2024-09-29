bf8fc001d43a73a51c6d817123e7a1bfd5663d52
HungTT912 <noohung2003@gmail.com>
2024-09-29 20:42:17 +0700

    a

--- train-bash.sh
+++ train-bash.sh
@@ -1,5 +1,1 @@
-python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-ant-highest.yaml \
-&python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-dkitty-highest.yaml 
-
-python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-highest.yaml \
-&python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind10-highest.yaml 
+python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-ant-highest.yaml 
