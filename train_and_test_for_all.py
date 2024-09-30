76da89afd2c0b85c5dd600a61ee81e95116e150d
HungTT912 <noohung2003@gmail.com>
2024-09-30 17:13:37 +0700

    a

--- train_and_test_for_all.py
+++ train_and_test_for_all.py
@@ -97,72 +97,72 @@ def main():
         nconfig.training.device = [torch.device(f"cuda:{gpu_ids}")]
     
     seed_list = range(8)
-    model_load_path_list = [] 
-    optim_sche_load_path_list = []
-    for seed in seed_list:
-        nconfig.args.train = True 
-        nconfig.args.seed = seed 
-        model_load_path, optim_sche_load_path = trainer(nconfig)
-        model_load_path_list.append(model_load_path) 
-        optim_sche_load_path_list.append(optim_sche_load_path)
+    # model_load_path_list = [] 
+    # optim_sche_load_path_list = []
+    # for seed in seed_list:
+    #     nconfig.args.train = True 
+    #     nconfig.args.seed = seed 
+    #     model_load_path, optim_sche_load_path = trainer(nconfig)
+    #     model_load_path_list.append(model_load_path) 
+    #     optim_sche_load_path_list.append(optim_sche_load_path)
     
-    # if nconfig.task.name != 'TFBind10-Exact-v0':
-    #     task = design_bench.make(nconfig.task.name)
-    # else:
-    #     task = design_bench.make(nconfig.task.name,
-    #                             dataset_kwargs={"max_samples": 10000})
-    # if task.is_discrete: 
-    #     task.map_to_logits()
-    # for eta in [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]: 
-    #     for w in [-1.5, -2.0, -2.5, -3.0 , -4.0]:  
-    #         for alpha in [0.8, 0.9, 0.95, 1.0]: 
-    #             results_100th = []
-    #             results_80th = [] 
-    #             results_50th = []
-    #             file_path = f'./few-shot-results/tuning_result_ant.csv'
+    if nconfig.task.name != 'TFBind10-Exact-v0':
+        task = design_bench.make(nconfig.task.name)
+    else:
+        task = design_bench.make(nconfig.task.name,
+                                dataset_kwargs={"max_samples": 10000})
+    if task.is_discrete: 
+        task.map_to_logits()
+    for eta in [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]: 
+        for w in [-1.5, -2.0, -2.5, -3.0 , -4.0]:  
+            for alpha in [0.8, 0.9, 0.95, 1.0]: 
+                results_100th = []
+                results_80th = [] 
+                results_50th = []
+                file_path = f'./few-shot-results/tuning_result_ant.csv'
 
-    #             if not os.path.isfile(file_path):
-    #                 with open(file_path, 'a') as file:
-    #                     header = ['eta','alpha','classifier_free_guidance_weight', 'mean (100th)', 'std (100th)', 'mean (80th)', 'std (80th)', 'mean (50th)', 'std (50th)']
-    #                     writer = csv.writer(file)
-    #                     writer.writerow(header)
-    #             for seed in seed_list: 
-    #                 nconfig.testing.eta = eta 
-    #                 nconfig.testing.classifier_free_guidance_weight = w 
-    #                 nconfig.testing.alpha = alpha 
-    #                 nconfig.args.train=False 
-    #                 nconfig.args.seed = seed
-    #                 nconfig.model.model_load_path = f'./results/few_shot/AntMorphology-Exact-v0/sampling_lr0.001/initial_lengthscale1.0/delta0.25/seed{seed}/BrownianBridge/checkpoint/top_model_epoch_100.pth'
-    #                 nconfig.model.optim_sche_load_path = f'ICLR25-few-shot-setting/results/few_shot/AntMorphology-Exact-v0/sampling_lr0.001/initial_lengthscale1.0/delta0.25/seed{seed}/BrownianBridge/checkpoint/top_optim_sche_epoch_100.pth'
-    #                 result = tester(nconfig,task)
-    #                 print("Score : ",result[0]) 
-    #                 results_100th.append(result[0])
-    #                 results_80th.append(result[1]) 
-    #                 results_50th.append(result[2])
-    #             assert len(results_100th)==8 
-    #             np_result_100th = np.array(results_100th)
-    #             mean_score_100th = np_result_100th.mean() 
-    #             std_score_100th = np_result_100th.std()
-    #             np_result_80th = np.array(results_80th)
-    #             mean_score_80th = np_result_80th.mean() 
-    #             std_score_80th = np_result_80th.std()
-    #             np_result_50th = np.array(results_50th)
-    #             mean_score_50th = np_result_50th.mean() 
-    #             std_score_50th = np_result_50th.std()
-    #             print(nconfig.task.name)
-    #             print(mean_score_100th, std_score_100th)
-    #             print(mean_score_80th, std_score_80th)
-    #             print(mean_score_50th, std_score_50th)
-    #             with open(file_path, 'a') as file:
-    #                 new_row = [eta, alpha, w, mean_score_100th, std_score_100th, mean_score_80th, std_score_80th, mean_score_50th, std_score_50th]
-    #                 writer = csv.writer(file)
-    #                 writer.writerow(new_row)
-    #                 df = pd.read_csv(file_path)
-    #                 table = wandb.Table(dataframe=df)
-    #                 wandb.log({"data_table": table})
+                if not os.path.isfile(file_path):
+                    with open(file_path, 'a') as file:
+                        header = ['eta','alpha','classifier_free_guidance_weight', 'mean (100th)', 'std (100th)', 'mean (80th)', 'std (80th)', 'mean (50th)', 'std (50th)']
+                        writer = csv.writer(file)
+                        writer.writerow(header)
+                for seed in seed_list: 
+                    nconfig.testing.eta = eta 
+                    nconfig.testing.classifier_free_guidance_weight = w 
+                    nconfig.testing.alpha = alpha 
+                    nconfig.args.train=False 
+                    nconfig.args.seed = seed
+                    nconfig.model.model_load_path = f'./results/few_shot/AntMorphology-Exact-v0/sampling_lr0.001/initial_lengthscale1.0/delta0.25/seed{seed}/BrownianBridge/checkpoint/top_model_epoch_100.pth'
+                    nconfig.model.optim_sche_load_path = f'ICLR25-few-shot-setting/results/few_shot/AntMorphology-Exact-v0/sampling_lr0.001/initial_lengthscale1.0/delta0.25/seed{seed}/BrownianBridge/checkpoint/top_optim_sche_epoch_100.pth'
+                    result = tester(nconfig,task)
+                    print("Score : ",result[0]) 
+                    results_100th.append(result[0])
+                    results_80th.append(result[1]) 
+                    results_50th.append(result[2])
+                assert len(results_100th)==8 
+                np_result_100th = np.array(results_100th)
+                mean_score_100th = np_result_100th.mean() 
+                std_score_100th = np_result_100th.std()
+                np_result_80th = np.array(results_80th)
+                mean_score_80th = np_result_80th.mean() 
+                std_score_80th = np_result_80th.std()
+                np_result_50th = np.array(results_50th)
+                mean_score_50th = np_result_50th.mean() 
+                std_score_50th = np_result_50th.std()
+                print(nconfig.task.name)
+                print(mean_score_100th, std_score_100th)
+                print(mean_score_80th, std_score_80th)
+                print(mean_score_50th, std_score_50th)
+                with open(file_path, 'a') as file:
+                    new_row = [eta, alpha, w, mean_score_100th, std_score_100th, mean_score_80th, std_score_80th, mean_score_50th, std_score_50th]
+                    writer = csv.writer(file)
+                    writer.writerow(new_row)
+                    df = pd.read_csv(file_path)
+                    table = wandb.Table(dataframe=df)
+                    wandb.log({"data_table": table})
     
-    # nconfig.args.train = False 
-    # wandb.finish() 
+    nconfig.args.train = False 
+    wandb.finish() 
     
 if __name__ == "__main__":
     main()
