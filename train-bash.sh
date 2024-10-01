d159a7ef0df1270f1747e72ad1359b9cb4fa7364
HungTT912 <noohung2003@gmail.com>
2024-10-01 15:25:57 +0700

    abs

--- train-bash.sh
+++ train-bash.sh
@@ -1,7 +1,12 @@
 python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind10-rand.yaml \
 &python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-dkitty-rand.yaml
 
-python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-17000.yaml \
-&python3 train_and_test_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-17000.yaml \
-&python3 train_and_test_for_all_2.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-17000.yaml \
-&python3 train_and_test_for_all_3.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-17000.yaml
+python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-tfbind10-rand.yaml \
+&python3 train_and_test_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind10-rand.yaml \
+&python3 train_and_test_for_all_2.py --config configs/few-shot-setting/Template-BBDM-tfbind10-rand.yaml \
+&python3 train_and_test_for_all_3.py --config configs/few-shot-setting/Template-BBDM-tfbind10-rand.yaml
+
+python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-dkitty-rand.yaml \
+&python3 train_and_test_for_all_1.py --config configs/few-shot-setting/Template-BBDM-dkitty-rand.yaml \
+&python3 train_and_test_for_all_2.py --config configs/few-shot-setting/Template-BBDM-dkitty-rand.yaml \
+&python3 train_and_test_for_all_3.py --config configs/few-shot-setting/Template-BBDM-dkitty-rand.yaml
