163740028702ec19f9b82cf0b91299febb4f3a0f
HungTT912 <noohung2003@gmail.com>
2024-10-01 14:15:37 +0700

    abs

--- train-bash-2.sh
+++ train-bash-2.sh
@@ -1,8 +1,4 @@
-python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-full-data.yaml \
-&python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-full-data.yaml \
-&python3 train_and_test_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-full-data.yaml \
+python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-full-data.yaml \
 &python3 train_and_test_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-full-data.yaml \
-&python3 train_and_test_for_all_2.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-full-data.yaml \
 &python3 train_and_test_for_all_2.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-full-data.yaml \
-&python3 train_and_test_for_all_3.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-full-data.yaml \
 &python3 train_and_test_for_all_3.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-full-data.yaml
