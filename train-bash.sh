da621a1f54c710774b2221227bf3ccfc6418f005
HungTT912 <noohung2003@gmail.com>
2024-10-01 12:57:22 +0700

    abs

--- train-bash.sh
+++ train-bash.sh
@@ -1,5 +1,3 @@
-python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-17000.yaml --save_top 
-
 
 python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-17000.yaml \
 &python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-17000.yaml \
