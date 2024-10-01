f9bb446823735b98ef0da6e62745f15f6377155c
HungTT912 <noohung2003@gmail.com>
2024-10-01 08:43:13 +0700

    abs2

--- train-bash-2.sh
+++ train-bash-2.sh
@@ -1,4 +1,12 @@
-python3 train_and_test_for_all_2.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg.yaml \
-&python3 train_and_test_for_all_2.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand.yaml \
-&python3 train_and_test_for_all_3.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg.yaml \
-&python3 train_and_test_for_all_3.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand.yaml
+python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-full-data.yaml --save_top \
+&python3 train_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-17000.yaml --save_top 
+
+
+python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-full-data.yaml \
+&python3 train_and_test_for_all.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-full-data.yaml \
+&python3 train_and_test_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-full-data.yaml \
+&python3 train_and_test_for_all_1.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-full-data.yaml \
+&python3 train_and_test_for_all_2.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-full-data.yaml \
+&python3 train_and_test_for_all_2.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-full-data.yaml \
+&python3 train_and_test_for_all_3.py --config configs/few-shot-setting/Template-BBDM-tfbind8-avg-full-data.yaml \
+&python3 train_and_test_for_all_3.py --config configs/few-shot-setting/Template-BBDM-tfbind8-rand-full-data.yaml
