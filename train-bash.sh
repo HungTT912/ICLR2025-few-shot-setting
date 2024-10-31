72358fa00f85288be5e8a157d274e63249ed44f4
HungTT912 <noohung2003@gmail.com>
2024-10-31 12:52:56 +0700

    expt

--- train-bash.sh
+++ train-bash.sh
@@ -1,4 +1,4 @@
 python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_2/Template-BBDM-ant-rand.yaml --save_top \
-# &python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_2/Template-BBDM-dkitty-rand.yaml --save_top \
-# &python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_2/Template-BBDM-tfbind8-rand-15000.yaml --save_top \
-# &python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_2/Template-BBDM-tfbind10-rand.yaml --save_top 
+&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_2/Template-BBDM-dkitty-rand.yaml --save_top \
+&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_2/Template-BBDM-tfbind8-rand-15000.yaml --save_top \
+&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_2/Template-BBDM-tfbind10-rand.yaml --save_top 
