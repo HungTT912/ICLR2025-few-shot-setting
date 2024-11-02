e8cc0cde902d374fa7d035bc27a987578c77c6b2
HungTT912 <noohung2003@gmail.com>
2024-11-02 18:33:15 +0700

    expt

--- train-bash.sh
+++ train-bash.sh
@@ -1,12 +1,10 @@
-python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l2.0.yaml --save_top \
-&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-dkitty-l2.0.yaml --save_top 
+python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l2.0.yaml \
+&python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l3.0.yaml \
+&python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l4.0.yaml 
 
+python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l5.0.yaml \
+&python3 test_for_dkitty_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-dkitty-l2.0.yaml \
+&python3 test_for_dkitty_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-dkitty-l3.0.yaml 
 
-python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l3.0.yaml --save_top \
-&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-dkitty-l3.0.yaml --save_top 
-
-python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l4.0.yaml --save_top \
-&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-dkitty-l4.0.yaml --save_top 
-
-python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l5.0.yaml --save_top \
-&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-dkitty-l5.0.yaml --save_top 
+python3 test_for_dkitty_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-dkitty-l4.0.yaml \
+&python3 test_for_dkitty_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-dkitty-l5.0.yaml 
