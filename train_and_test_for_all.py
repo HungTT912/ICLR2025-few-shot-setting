7ba5a244fb108a15a01295e4dc8c773aee36bbae
HungTT912 <noohung2003@gmail.com>
2024-11-02 09:23:40 +0700

    tune 3

--- train_and_test_for_all.py
+++ train_and_test_for_all.py
@@ -123,10 +123,10 @@ def main():
             writer.writerow(header)
     df = pd.read_csv(file_path) 
     lengthscale = nconfig.GP.initial_lengthscale
-    tested_params = df[['eta','alpha','classifier_free_guidance_weight']].to_numpy()
+    tested_params = df[['eta','alpha','classifier_free_guidance_weight']].values.tolist()
     for eta in [0.0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.5]: 
         for w in [-1.5, -1, -0.5, 0, 0.5, 1, 1.5, 2 , 2.5 , 3, 4]:  
-            for alpha in [0.8,0.85,0.9,0.95,1.0]: 
+            for alpha in [0.8]: 
                 results_100th = []
                 results_80th = [] 
                 results_50th = []
