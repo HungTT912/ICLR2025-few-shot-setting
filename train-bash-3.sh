cc8637c54d34d37bfe72bc8331c8345fc889640b
HungTT912 <noohung2003@gmail.com>
2024-11-03 19:18:54 +0700

    tune 3'

--- train-bash-3.sh
+++ train-bash-3.sh
@@ -1,8 +1,8 @@
-python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l6.0-d1.0.yaml --save_top \
-&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l6.0-d2.0.yaml --save_top \
+# python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l6.0-d1.0.yaml --save_top \
+# &python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l6.0-d2.0.yaml --save_top 
 
 python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l6.0-d1.0yaml \
 &python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l6.0-2.0.yaml \
-&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l6.0-d0.5.yaml --save_top \
+&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l6.0-d0.5.yaml --save_top 
 
 python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_3/Template-BBDM-ant-l6.0-d0.5yaml
