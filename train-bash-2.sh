ae2dc08f42329e83fe91989914b737d7673a4d9c
HungTT912 <noohung2003@gmail.com>
2024-11-05 00:04:14 +0700

    tune 4

--- train-bash-2.sh
+++ train-bash-2.sh
@@ -1,3 +1,3 @@
-python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_4/Template-BBDM-ant-l7.5-dl1.5-v5.5-dv2.5.yaml --save_top \
-&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_4/Template-BBDM-ant-l7.5-dl1.5-v5.5-dv3.5.yaml --save_top \
-&python3 train_and_test_for_all.py --config configs/few-shot-setting/tune_4/Template-BBDM-ant-l7.5-dl1.5-v5.5-dv4.5.yaml --save_top 
+python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_4/Template-BBDM-ant-l7.5-dl1.5-v5.5-dv2.5.yaml --save_top \
+&python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_4/Template-BBDM-ant-l7.5-dl1.5-v5.5-dv3.5.yaml --save_top \
+&python3 test_for_ant_tune_3.py --config configs/few-shot-setting/tune_4/Template-BBDM-ant-l7.5-dl1.5-v5.5-dv4.5.yaml --save_top 
