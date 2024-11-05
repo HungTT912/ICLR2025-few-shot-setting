24ded6477d51f91e0c06cb140bf76a39392435bf
HungTT912 <noohung2003@gmail.com>
2024-11-05 21:48:24 +0700

    tune 5

--- runners/BaseRunner.py
+++ runners/BaseRunner.py
@@ -366,13 +366,15 @@ class BaseRunner(ABC):
                                 variance=variance,
                                 noise = base_noise,
                                 mean_prior=mean_prior,
-                                delta_lengthscale=self.config.GP.delta_lengthscale,
-                                delta_variance=self.config.GP.delta_variance)
+                                delta_lengthscale=self.config.base_GP.delta_lengthscale,
+                                delta_variance=self.config.base_GP.delta_variance)
             base_gp_hyper = GP_hyper(device=self.config.training.device[0],
                                 lengthscale=base_lengthscale,
                                 variance=base_variance,
                                 noise = base_noise,
-                                mean_prior=mean_prior)
+                                mean_prior=mean_prior,
+                                delta_lengthscale=self.config.GP.delta_lengthscale,
+                                delta_variance=self.config.GP.delta_variance)
 
             val_loader = None
             val_dataset = []
